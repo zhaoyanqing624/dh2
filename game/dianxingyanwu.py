ce8f221bce05b51bea401cc8e7aaf96a0c9136db
zhaoyanqing624 <77528570@qq.com>
2019-11-19 13:23:36 +0800

    2019年11月19日 12:48:52

--- game/dianxingyanwu.py
+++ game/dianxingyanwu.py
@@ -5,6 +5,7 @@ from system.keyboard import KeyBoard
 from system.screen import Screen
 from system.mouse import Mouse
 import _Tools.getFighting
+import pyautogui
 class DianXing():
     def __init__(self):
         self.transform = TransForm()
@@ -56,18 +57,28 @@ class DianXing():
                 else:
                     if pos_caozuo is not 0:
                         # 星将
+                        flag = 0
                         while True:
                             time.sleep(1)
                             self.screen.cut_screen()
                             pos_caozuo2= self.screen.get_location_picture("D:\\dh2\\game\\system\\caozuo.png")
                             loc_begin2 = self.screen.get_location_picture("D:\\dh2\\game\\dianxing\\begin.png")
                             if pos_caozuo2 is not 0:
-                                for z in range(5):
-                                    self.keyboard.press_shortcut_key('alt', 'a')
-                                    self.keyboard.press_shortcut_key('alt', 'w')
-                                    self.mouse.click_element(314, 179, times=0.3)
-                                    self.mouse.click_element(198, 344, times=0.3)
-                                    self.common.change_teamer(0.3)
+                                if flag is 0:
+                                    for z in range(5):
+                                        self.keyboard.press_shortcut_key('alt', 'a')
+                                        self.keyboard.press_shortcut_key('alt', 'w')
+                                        self.mouse.click_element(314, 179, times=0.3,right=True)
+                                        self.keyboard.press_shortcut_key('alt', 's')
+                                        self.mouse.click_element(198, 344, times=0.3)
+                                        self.common.change_teamer(0.3)
+                                        flag+=1
+                                else:
+                                    for z in range(5):
+                                        self.keyboard.press_shortcut_key('alt', 'a')
+                                        self.keyboard.press_shortcut_key('alt', 's')
+                                        pyautogui.click()
+                                        self.common.change_teamer(0.3)
                             if loc_begin2 is not 0:
                                 break
 
