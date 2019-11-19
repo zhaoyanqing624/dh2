d4bb3e43a53215d392d8aef9706d57c1a830931c
zhaoyanqing624 <77528570@qq.com>
2019-11-19 21:57:42 +0800

    2019年11月19日 20:08:42

---
+++ game/shuilu.py
@@ -0,0 +1,31 @@
+import time
+from game.common import Common
+from system.transform import TransForm
+from system.keyboard import KeyBoard
+from system.screen import Screen
+from system.mouse import Mouse
+import _Tools.getFighting
+import pyautogui
+class ShuiLu():
+    def __init__(self):
+        self.transform = TransForm()
+        self.common = Common()
+        self.keyboard = KeyBoard()
+        self.screen = Screen()
+        self.mouse = Mouse()
+
+    def task_start(self):
+        self.screen.find_ele_picture('game\\system\\zidong')
+        time.sleep(3)
+        for i in range(5):
+            self.keyboard.press_key('f7')
+            if i is 0:
+                self.mouse.click_element(198, 338, times=0.3)
+            else:
+                pyautogui.click()
+            self.keyboard.press_shortcut_key('alt', 'a')
+            self.keyboard.press_shortcut_key('alt', '8')
+            self.common.change_teamer(0.3)
+if __name__ == '__main__':
+    # 开始
+    ShuiLu().task_start()
